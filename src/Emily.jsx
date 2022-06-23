3f48576992584b973a46771a05462b6d7e1df75d
“emilykiss” <emilylanekiss@gmail.com>
2022-06-23 14:13:45 -0700

    emily

---
+++ src/Emily.jsx
@@ -0,0 +1,8 @@
+export default function Emily(){
+    return (
+      <div>
+        <h1>I AM THE EMILY COMPONENT!</h1>
+        <img src="https://www.mspca.org/wp-content/uploads/2016/01/pug-small-shutterstock_139507241.jpg"></img>
+      </div>
+    )
+}
