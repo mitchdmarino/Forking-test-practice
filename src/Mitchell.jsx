af82d0b6a76b2bcae5af8dd1b8c69ac0e503a766
mitchdmarino <mitchdmarino@gmail.com>
2022-06-23 14:29:19 -0700

    adds bio to Mitchell

--- src/Mitchell.jsx
+++ src/Mitchell.jsx
@@ -2,7 +2,8 @@ export default function Mitchell() {
     return (
         <>
             Hello Mitchell
-            <img src='https://i.imgur.com/zDoSZAB.jpg?1'></img>
+            Bio: lorem ipsum dolor etc 
+           
         </>
     )
 }
